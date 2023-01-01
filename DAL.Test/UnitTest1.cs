01ff4bb7477f36094af6a6ea0eb0360f5f107d38
irakiw <irakiw03@gmail.com>
2023-01-02 00:47:13 +0200

    test add del test2

---
+++ DAL.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DAL.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- DAL/UnitTest1.cs
+++ DAL/UnitTest1.cs
@@ -1,4 +1,4 @@
-namespace DAL.Tests
+namespace DAL
 {
     public class UnitTest1
     {
