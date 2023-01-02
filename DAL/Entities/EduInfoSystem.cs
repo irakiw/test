c4ea1c6182c51e63d5281d2b9a930fcf12969ddf
irakiw <irakiw03@gmail.com>
2023-01-02 03:27:50 +0200

    add clases fake unit

---
+++ DAL/Entities/EduInfoSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DAL.Entities
+{
+    class EduInfoSystem
+    {
+        public List<Desk> InformationDesks { get; set; }
+        public List<Group> UserGroups { get; set; }
+    }
+}
